b7102576c69a950f27df89b1232158ff800b4603
Saident <averilprimayuda@gmail.com>
2022-06-12 20:33:10 +0700

    Updated home on login

--- app/src/main/java/com/example/mindly/HomeActivity.java
+++ app/src/main/java/com/example/mindly/HomeActivity.java
@@ -6,13 +6,19 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.textclassifier.TextClassification;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -22,13 +28,41 @@ public class HomeActivity extends AppCompatActivity {
     PostAdapter postAdapter;
     ArrayList<PostModel> listPost;
 
-    FirebaseAuth mAuth;
+    FirebaseUser user;
+    private String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
 
+        recView();
+
+        //userlogin
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        database = FirebaseDatabase.getInstance().getReference("Users");
+        userID = user.getUid();
+
+        final TextView tv_name = (TextView) findViewById(R.id.tv_name);
+        database.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user = snapshot.getValue(User.class);
+                if (user != null){
+                    String fullname = user.fullname;
+                    tv_name.setText("Hi, " + fullname + "!");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(HomeActivity.this, "Something wrong happened!", Toast.LENGTH_SHORT);
+            }
+        });
+    }
+
+    private void recView(){
+        //recview
         rv_post = findViewById(R.id.recview_post);
         database = FirebaseDatabase.getInstance().getReference("PostData");
         rv_post.setHasFixedSize(true);
